fb9435765a35e95a918c7da1a85d9680818f5443
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-02 16:06:47 +0800

    fix main login

--- caoliu/__main__.py
+++ caoliu/__main__.py
@@ -5,14 +5,18 @@ if __name__ == "__main__":
     # init database
     db.init()
 
-    # apan login
+    # get apan config
     username = config.get('apan', 'User')
     password = config.get('apan', 'Pass')
     apan_url = config.get('apan', 'Url')
-    apan_browser = apan.login(username, password, apan_url)
 
-    # run crawel spider
+    # get caoliu config
     base_url = config.get('t66y', 'BaseUrl')
     start_url = base_url + '/thread0806.php?fid=20'
+
+    # run crawel spider 10 times
     for i in range(10):
+        # apan login
+        apan_browser = apan.login(username, password, apan_url)
+        # crawel run
         crawel.run(start_url, apan_browser)
