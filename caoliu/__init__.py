fc55d0335686d3e46ea221c90ab6d46c280cc909
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-11 10:40:10 +0800

    change log have color

--- caoliu/__init__.py
+++ caoliu/__init__.py
@@ -5,7 +5,7 @@ from loguru import logger
 
 base_path = os.path.split(os.path.realpath(__file__))[0]
 
-logger.add(os.path.join(base_path, 'output.log'), encoding='utf-8')
+logger.add(os.path.join(base_path, 'output.log'), colorize=True, encoding='utf-8')
 
 if sys.version_info < (3, 6):
     raise RuntimeError("Only Python 3.6+ is supported.")
