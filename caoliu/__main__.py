7d04c1d59e3c99869701d030c7e4476edc94e186
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-26 14:57:06 +0800

    update random start url

--- caoliu/__main__.py
+++ caoliu/__main__.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 from caoliu import apan, crawel, db, logger
 from util import config
@@ -10,9 +11,11 @@ if __name__ == "__main__":
     # get caoliu config
     base_url = config.get('t66y', 'BaseUrl')
     start_url = base_url + '/thread0806.php?fid=20'
+    urls = [start_url + '&search=&page=' + str(i) for i in range(1, 25)]
 
     # run crawel spider 10 times
     for i in range(10):
         # crawel run
         time.sleep(20)
-        crawel.run(start_url)
+        url = random.choice(urls)
+        crawel.run(url)
