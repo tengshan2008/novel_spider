4155230b9a3a715e5c51b24f0d324faa1e9e9959
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-28 17:29:52 +0800

    update debug

--- caoliu/crawel.py
+++ caoliu/crawel.py
@@ -114,7 +114,7 @@ def is_end_page(browser):
     if browser.find(class_='pages') is None:
         logger.debug('no pages detail: {}', browser.url)
         for state in browser._states:
-            logger.debug(state.response.content.decode('utf-8', 'replace'))
+            logger.debug(state.response.text)
         return True
 
     for label_a in browser.find_all('a'):
