f388a73ed3423a676eac50684a1085c497b6237b
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-30 15:14:34 +0800

    update test

--- tests/test.py
+++ tests/test.py
@@ -1,7 +1,19 @@
 from robobrowser import RoboBrowser
 import requests
 
-url = 'https://hs.etet.men/thread0806.php?fid=20&search=&page=6'
+url = 'https://hs.etet.men/read.php?tid=3500394&page=2'
+
+# accept-encoding: gzip, deflate, br
+
+# accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3
+# accept-encoding: gzip, deflate, br
+# accept-language: zh-CN,zh;q=0.9
+# cache-control: no-cache
+# cookie: __cfduid=daf1c243a21cc49f4807f162bc3a5ad6d1556430257; UM_distinctid=16a6278ff9b658-00d91e85434a76-7a1b34-144000-16a6278ff9c815; 227c9_lastvisit=0%091556608160%09%2Fread.php%3Ftid%3D3500394%26page%3D1; CNZZDATA950900=cnzz_eid%3D1797340401-1556428250-%26ntime%3D1556606147
+# pragma: no-cache
+# referer: https://hs.etet.men/htm_data/20/1904/3500394.html
+# upgrade-insecure-requests: 1
+# user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36
 
 def open():
     session = requests.Session()
@@ -9,10 +21,10 @@ def open():
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
         'accept-language': 'zh-CN,zh;q=0.9',
         'cache-control': 'no-cache',
-        'cookie': '__cfduid=daf1c243a21cc49f4807f162bc3a5ad6d1556430257; UM_distinctid=16a6278ff9b658-00d91e85434a76-7a1b34-144000-16a6278ff9c815; 227c9_lastvisit=0%091556591446%09%2Fread.php%3Ftid%3D3337712%26page%3D2; CNZZDATA950900=cnzz_eid%3D1797340401-1556428250-%26ntime%3D1556590258',
+        'cookie': '__cfduid=daf1c243a21cc49f4807f162bc3a5ad6d1556430257; UM_distinctid=16a6278ff9b658-00d91e85434a76-7a1b34-144000-16a6278ff9c815; CNZZDATA950900=cnzz_eid%3D1797340401-1556428250-%26ntime%3D1556601058; 227c9_lastvisit=0%091556604559%09%2Fread.php%3Ftid%3D3510604%26page%3D2',
         'pragma': 'no-cache',
+        'referer': 'https://hs.etet.men/htm_data/20/1904/3500394.html',
         'upgrade-insecure-requests': '1',
-        'referer': 'https://hs.etet.men/thread0806.php?fid=20',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
     }
 
@@ -23,7 +35,7 @@ def open():
     
     print(browser.session.headers)
 
-    # print(browser.parsed()[0])
+    print(browser.parsed()[0])
 
 if __name__ == "__main__":
     open()
