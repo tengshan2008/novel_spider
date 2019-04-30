d92f2ed073a339a1b1510f80d35e05553aa2a97d
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-30 10:09:46 +0800

    add user agent

--- tests/test.py
+++ tests/test.py
@@ -2,14 +2,12 @@ from robobrowser import RoboBrowser
 
 url = 'https://hs.etet.men/read.php?tid=3337712&page=2'
 
-
+user_agent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit'
 
 def open():
-    browser = RoboBrowser(history=True, parser='html5lib', timeout=30)
+    browser = RoboBrowser(history=True, parser='html5lib', 
+                          timeout=30, user_agent=user_agent)
     browser.open(url)
-
-    browser.session.cookies['_gh_sess']
-    browser.session.headers['User-Agent']
 
     print(browser.parsed())
 
