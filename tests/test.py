89313bee6bdf0254668cebc2fb789104684a3fd9
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-30 10:02:54 +0800

    add session option

--- tests/test.py
+++ tests/test.py
@@ -2,9 +2,14 @@ from robobrowser import RoboBrowser
 
 url = 'https://hs.etet.men/read.php?tid=3337712&page=2'
 
+
+
 def open():
     browser = RoboBrowser(history=True, parser='html5lib', timeout=30)
     browser.open(url)
+
+    browser.session.cookies['_gh_sess']
+    browser.session.headers['User-Agent']
 
     print(browser.parsed())
 
