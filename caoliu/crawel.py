154988adbf74419393dbd8288ff11b6b12035e1f
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-30 11:01:10 +0800

    update session

--- caoliu/crawel.py
+++ caoliu/crawel.py
@@ -19,9 +19,20 @@ TODAY = '今天'
 YESTERDAY = '昨天'
 PATTERN = '草榴官方客戶端|來訪者必看的內容|发帖前必读|关于论坛的搜索功能|文学区违规举报专贴|文區版規'
 
+session = requests.Session()
+session.headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'cache-control': 'no-cache',
+    'cookie': '__cfduid=daf1c243a21cc49f4807f162bc3a5ad6d1556430257; UM_distinctid=16a6278ff9b658-00d91e85434a76-7a1b34-144000-16a6278ff9c815; 227c9_lastvisit=0%091556591446%09%2Fread.php%3Ftid%3D3337712%26page%3D2; CNZZDATA950900=cnzz_eid%3D1797340401-1556428250-%26ntime%3D1556590258',
+    'pragma': 'no-cache',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+}
 
 def run(url : str, idx : int):
-    browser = RoboBrowser(parser='html5lib', history=True,
+    browser = RoboBrowser(parser='html5lib', history=True, session=session,
                           timeout=30, tries=5, multiplier=0.3)
 
     try:
@@ -178,7 +189,7 @@ def get_link(novel : Tag) -> str:
 
 
 def get_content(info : dict) -> str:
-    browser = RoboBrowser(parser='html5lib', history=True,
+    browser = RoboBrowser(parser='html5lib', history=True, session=session,
                           timeout=30, tries=5, multiplier=0.3)
     try:
         browser.open(info['link'])
