abb1d48b7826f1dac41a78ec4092cedc90925f69
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-30 10:37:40 +0800

    update session

--- tests/test.py
+++ tests/test.py
@@ -3,30 +3,24 @@ import requests
 
 url = 'https://hs.etet.men/read.php?tid=3337712&page=2'
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
-# user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
-
 def open():
-    # session = requests.Session()
-    # session.headers = {
-    #     'Date': 'Tue, 30 Apr 2019 02:16:04 GMT', 
-    #     'Content-Type': 'text/html', 
-    #     'Transfer-Encoding': 'chunked', 
-    #     'Connection': 'keep-alive', 
-    #     'Set-Cookie': '__cfduid=d811bb4f9b359dd06254550b9c855c4af1556590564; expires=Wed, 29-Apr-20 02:16:04 GMT; path=/; domain=.etet.men; HttpOnly; Secure, 227c9_lastvisit=0%091556590564%09%2Fread.php%3Ftid%3D3337712%26page%3D2; expires=Wed, 29-Apr-2020 02:16:04 GMT; Max-Age=31536000; path=/',
-    #     'X-Powered-By': 'PHP/5.6.33',
-    #     'Vary': 'Accept-Encoding',
-    #     'Expect-CT': 'max-age=604800, report-uri="https://report-uri.cloudflare.com/cdn-cgi/beacon/expect-ct"', 
-    #     'Server': 'cloudflare', 
-    #     'CF-RAY': '4cf602f1cb5e7892-LAX', 
-    #     'Content-Encoding': 'gzip'
-    # }
+    session = requests.Session()
+    session.headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cache-control': 'no-cache',
+        'cookie': '__cfduid=daf1c243a21cc49f4807f162bc3a5ad6d1556430257; UM_distinctid=16a6278ff9b658-00d91e85434a76-7a1b34-144000-16a6278ff9c815; 227c9_lastvisit=0%091556591446%09%2Fread.php%3Ftid%3D3337712%26page%3D2; CNZZDATA950900=cnzz_eid%3D1797340401-1556428250-%26ntime%3D1556590258',
+        'pragma': 'no-cache',
+        'upgrade-insecure-requests': 1,
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36'
+    }
 
     browser = RoboBrowser(history=True, parser='html5lib', 
-                          timeout=30, user_agent=user_agent)
+                          timeout=30, session=session)
     browser.open(url)
     
-    # print(browser.state.response.headers)
+    print(browser.state.response.headers)
 
     print(browser.parsed()[0])
 
