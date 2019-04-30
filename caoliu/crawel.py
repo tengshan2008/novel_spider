89be9a0022cbf7a0bd31796b3a6ef7479b68f76b
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-04-30 11:14:00 +0800

    update debug

--- caoliu/crawel.py
+++ caoliu/crawel.py
@@ -137,7 +137,7 @@ def is_end_page(browser : RoboBrowser) -> bool:
 
     if browser.find(class_='pages') is None:
         logger.debug('no pages detail: {}', browser.url)
-        logger.debug('response: {}', browser.state.response.text)
+        logger.debug('response: {}', browser.state.response.content.decode('gbk'))
         logger.debug('beautiful soup parse: {}', browser.parsed())
         return True
 
