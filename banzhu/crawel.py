f260dc4ea611eab77c940e02e64ae09789049b94
yanghetengshan <yanghetengshan@sayinfo.cn>
2019-05-17 18:33:55 +0800

    add pipline package add banzhu crawel moduel

--- banzhu/crawel.py
+++ banzhu/crawel.py
@@ -1,99 +1,166 @@
 """crawel 001banzhu website novels
 """
 
-
-import random
-import time
+import datetime
 import os
+import random
+import re
+import time
+import string
 
+import requests
+from bs4 import BeautifulSoup
+from bs4.element import Tag
 from robobrowser import RoboBrowser
 
 from banzhu import db, logger
+from pipline import apan
 from util import config
-import os
 
 base_path = os.path.split(os.path.realpath(__file__))[0]
 
 
-def run():
-    browser = RoboBrowser(parser='html.parser', history=True,
-                          timeout=30, tries=5)
+def run(url: str):
+    browser = RoboBrowser(parser='html5lib', history=True,
+                          timeout=30, tries=5, multiplier=0.3)
+
+    try:
+        browser.open(url)
+    except (requests.exceptions.Timeout, requests.ConnectionError) as e:
+        logger.error('request fail {url}, {err}', url=url, err=e)
+        return
+    except:
+        logger.exception('request failed: {url}', url=url)
 
     db_file = config.get('sqlite', 'banzhu')
-    # dbase = db.get(db_file)
+    dbase = db.get(db_file)
 
-    base_url = config.get('banzhu', 'BaseUrl')
-    novel_links = []
-    ids_path = os.path.join(base_path, 'ids.txt')
-    with open(ids_path, 'r') as f:
-        for novel_id in f.readlines():
-            novel_links.append(base_url + novel_id)
-
-    for novel_link in novel_links:
-        time.sleep(random.randint(2, 5))
-
+    count = 1
+    while True:
+        logger.info("current page is {}", count)
+        for novel in get_novels(browser):
+            time.sleep(random.randint(2, 5))
+            novel_info = get_novel_info(novel)
+            novel_content = get_novel_content(novel_info)
+            novel_info['size'] = str(len(novel_content))
+            if db.insert(novel_info, dbase):
+                ok = apan.upload(novel_info, novel_content)
+                if not ok:
+                    db.delete(novel_info, dbase)
+        if is_end_page(browser):
+            break
         try:
-            browser.open(novel_link.strip())
-        except Exception as e:
-            logger.error('request failed: {}\nerror: {}', novel_link, e)
-            continue
-        else:
-            get_novel(browser, "")
+            browser.follow_link(next_page(browser))
+        except (requests.exceptions.Timeout, requests.ConnectionError) as e:
+            logger.error()
+            db.close(dbase)
+            return
+        except:
+            logger.exception('request failed: {url}', url=browser.url)
+            db.close(dbase)
+            return
+        count += 1
+    
+    db.close(dbase)
 
-    # db.close(dbase)
+def is_end_page(browser: RoboBrowser) -> bool:
+    if browser.state.response.content is None:
+        logger.warning("request content failed")
+        return True
+
+    pagelink = browser.find(class_='pagelink')
+    if pagelink is None:
+        logger.warning('no pages url: {}', browser.url)
+        fid = ''.join(random.sample(string.ascii_letters + string.digits, 8))
+        logger.warning('response detail in: {}', fid+'.html')
+        fail_file_path = os.path.join(base_path, 'pages', fid+'.html')
+        with open(fail_file_path, 'wb') as f:
+            f.write(browser.state.response.content)
+        return True
+    
+    if pagelink.find(class_='next') is None:
+        return True
+
+    return False
+
+def next_page(browser: RoboBrowser) -> Tag:
+    pagelink = browser.find(class_='pagelink')
+    return pagelink.find(class_='next')
 
 
-def get_novel(browser, dbase):
-    logger.info("link {}", browser.url)
-    info = browser.find(id='info')
-    logger.info(info)
-    novel_info = {
-        'title': get_title(info),
-        'author': get_author(info),
-        'last_update': get_last_update(info)
+def get_novels(browser: RoboBrowser):
+    return [li for li in browser.find(class_="l").ul][1:-1]
+
+
+def get_id(novel: Tag):
+    return novel.find(class_='s2').a['href'].split('/')[3]
+
+def get_title(novel: Tag):
+    return novel.find(class_='s2').a.string
+
+def get_author(novel: Tag):
+    return novel.find(class_='s5').string
+
+def get_date(novel: Tag):
+    return novel.find(class_='s3').contents[1]
+
+def get_link(novel: Tag):
+    return novel.find(class_='s2').a['href']
+
+def get_novel_info(novel: Tag):
+    return {
+        'id': get_id(novel), 'title': get_title(novel),
+        'author': get_author(novel), 'date': get_date(novel),
+        'link': get_link(novel), 'chapter': '0/0',
     }
 
-    chapters = browser.find(id='list').find_all('a')
-    content = get_content(chapters)
-    logger.info(content)
-    # if db.insert(novel_info, dbase):
-    #     apan.upload(novel_info, content)
+def get_novel_content(info):
+    browser = RoboBrowser(parser='html5lib', history=True,
+                          timeout=30, tries=5, multiplier=0.3)
 
-
-def get_title(info):
-    return info.find('h1').string
-
-
-def get_author(info):
-    return info.find_all('p')[0].string
-
-
-def get_last_update(info):
-    return info.find_all('p')[2].string
-
-
-def get_content(chapters):
-    browser = RoboBrowser(parser='html.parser', history=True,
-                          timeout=30, tries=5)
-    content = []
-    for chapter in chapters:
-        time.sleep(5)
-
-        link = config.get('banzhu', 'BaseUrl') + chapter['href']
+    try:
+        browser.open(info['link'])
+    except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
+        logger.error('', url=info['link'], err=e)
+        return ''
+    except:
+        logger.exception('request failed: {url}', url=info['link'])
+        return ''
+    
+    chapter_contents = []
+    chapters_link = get_chapters_link(browser)
+    chapter_total = len(chapters_link)
+    chapter_count = 1
+    for chapter_link in chapters_link:
+        time.sleep(random.randint(2, 5))
         try:
-            browser.open(link)
+            browser.follow_link(chapter_link)
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError) as e:
+            logger.error('', url=browser.url, err=e)
+            break
         except:
-            logger.error('request failed:', link)
-            continue
+            logger.exception('request failed: {url}', url=browser.url)
+            break
+        chapter_contents.append(get_chapter_content(browser))
+        chapter_count += 1
+    
+    if len(chapter_contents) == 0:
+        logger.info('void novel detail is \n{}', browser.find())
+    
+    info['chapter'] = str(chapter_count) + '/' + str(chapter_total)
 
-        content.append('chapter: ' + chapter.string)
-        content += get_chapter_content(browser)
-
-    return '\n'.join(content)
+    return '\n'.join(chapter_contents)
 
 
-def get_chapter_content(browser):
-    content = browser.find(id='content')
-    if content is None:
-        return []
-    return [line.strip('\n') for line in content.strings]
+def get_chapters_link(browser: RoboBrowser):
+    chapters_link = []
+    for d in set(browser.find(id="list").dl.contents):
+        if d.name == 'dd':
+            chapters_link.append(d.a)
+    return chapters_link
+
+
+def get_chapter_content(browser: RoboBrowser):
+    chapter_name = browser.find(class_="bookname").h1.string
+    chapter_content = '\n'.join(filter(lambda x: x != '', [line.strip() for line in browser.find(id="content").strings]))
+    return chapter_name + '\n' + chapter_content
---
+++ pipline/apan.py
@@ -0,0 +1,157 @@
+"""save novel content into anyview website
+"""
+import time
+from os import path, remove
+
+import requests
+from robobrowser import RoboBrowser, forms
+
+from loguru import logger
+from util import config
+
+base_path = path.split(path.realpath(__file__))[0]
+RequestsFail = 'requests failed: {url}\nconnetion error: {err}'
+
+def login(username, password, url):
+    browser = RoboBrowser(parser='html.parser', history=True,
+                          timeout=30, tries=5, multiplier=0.3)
+
+    try:
+        browser.open(url)
+    except requests.ConnectionError as e:
+        logger.error(RequestsFail, url=url, err=e)
+        return None
+    except:
+        logger.error('request failed: {url}', url=url)
+        logger.exception('detail')
+        return None
+
+    # login
+    login_form = browser.get_form(id='log-in')
+    login_form['account'].value = username
+    login_form['password'].value = password
+    browser.submit_form(login_form)
+    # access account
+    account = browser.find(href='/account')
+    if account is None:
+        logger.info("page detail is: \n{}", browser.find())
+        return None
+    try:
+        browser.follow_link(account)
+    except requests.ConnectionError as e:
+        logger.error(RequestsFail, url=browser.url, err=e)
+        return None
+    except:
+        logger.error('request failed: {url}', url=browser.url)
+        logger.exception('detail')
+        return None
+
+    return browser
+
+
+def upload(novel_info, content):
+    # get apan config
+    username = config.get('apan', 'User')
+    password = config.get('apan', 'Pass')
+    apan_url = config.get('apan', 'Url')
+    # login
+    browser = login(username, password, apan_url)
+    if browser is None:
+        logger.error("login apan failed")
+        time.sleep(10)
+        return False
+    # delete
+    browser = delete(browser, novel_info['title'])
+    if browser is None:
+        logger.error("delete novel failed")
+        time.sleep(10)
+        return False
+    upload_form = browser.get_forms()[0]
+    # add upload action field
+    upload_action_str = '<input type="hidden" \
+        name="action" value="upload_file_post" />'
+    upload_action = forms.fields.Input(upload_action_str)
+    upload_form.add_field(upload_action)
+    # add upload file field
+    pth = path.join(base_path, 'novels', novel_info['title'] + '.txt')
+    # submit upload form
+    try:
+        with open(pth, 'w', encoding='utf-8') as f:
+            f.write(file_content(novel_info, content))
+    except (OSError, IOError) as e:
+        logger.error('write file error: {}', e)
+        return False
+    except:
+        logger.exception('detail')
+        return False
+    try:
+        with open(pth, 'r', encoding='utf-8') as f:
+            upload_form['file_to_upload'].value = f
+            browser.submit_form(upload_form)
+    except requests.ConnectionError as e:
+        logger.error(RequestsFail, url=browser.url, err=e)
+        return False
+    except (OSError, IOError) as e:
+        logger.error('read file error: {}', e)
+        return False
+    except:
+        logger.exception("detail")
+        return False
+    # delete temp file
+    write_in_local = config.getboolean('app', 'WriteInLocal')
+    if not write_in_local and path.exists(pth):
+        remove(pth)
+
+    return True
+
+
+def delete(browser, title):
+    form = browser.get_forms()[1]
+    # find old file and delete it
+    table = browser.find(class_='table table-bordered table-striped')
+    if table is None:
+        logger.info("page detail is: \n{}", browser.find())
+        return None
+    for tr in table.tbody.find_all('tr'):
+        if title == tr.find('td', class_="item-title").string[1:-4]:
+            button = tr.find('button')
+            nid = button['value']
+            delete_button_str = str(button)
+            delete_action = forms.fields.Input(delete_button_str)
+            form.add_field(delete_action)
+            form['file_id'].value = nid
+            try:
+                browser.submit_form(form)
+                return browser
+            except requests.ConnectionError as e:
+                logger.error(RequestsFail, url=browser.url, err=e)
+                return None
+            except:
+                logger.error('request failed: {url}', url=browser.url)
+                logger.exception("detail")
+                return None
+
+    return browser
+
+
+def file_content(novel_info, content):
+    return '\n'.join([
+        '标题：' + novel_info['title'],
+        '作者：' + novel_info['author'],
+        '类型：' + novel_info['type'],
+        '日期：' + novel_info['date'],
+        content
+    ])
+
+
+if __name__ == "__main__":
+    ni = {
+        'id': 123,
+        'title': '测试文件',
+        'author': '大西瓜',
+        'date': '2019-3-6',
+        'type': '其他',
+        'link': 'http://www.example.com'
+    }
+    content = '这是一个新的abc测试文件。\n'
+    upload(ni, content)
