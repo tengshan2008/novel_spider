ac6947ede01aa69318ba8f0e1e5d1d5136fb86d1
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-03-09 17:42:17 +0800

    update start

--- novel/crawel.py
+++ novel/crawel.py
@@ -80,8 +80,8 @@ class Crawl(object):
 
 
 if __name__ == "__main__":
-    # crawl = Crawl('https://cl.330f.tk/thread0806.php?fid=20&search=&page=1')
-    # crawl.start()
-    page = Page('https://cl.330f.tk/thread0806.php?fid=20&search=&page=1', 1)
-    for i in page.get_items():
-        print(i)
+    crawl = Crawl('https://cl.330f.tk/thread0806.php?fid=20&search=&page=1')
+    crawl.start()
+    # page = Page('https://cl.330f.tk/thread0806.php?fid=20&search=&page=1', 1)
+    # for i in page.get_items():
+    #     print(i)
