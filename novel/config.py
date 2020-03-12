dbc4c51b1d9a139c089743c00fa68df9b7dd0ead
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-03-12 18:23:36 +0800

    update path

--- novel/config.py
+++ novel/config.py
@@ -4,5 +4,5 @@ USER_AGENT = """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/\
 537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"""
 
 # store
-DAV_PATH = "/mnt/DAV/book"
+DAV_PATH = "/media/share/novel/books"
 DB_FILE = "book.db"
