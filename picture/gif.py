d7683d70697c8975c271bf936bac84eae0ad7634
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-03-16 11:13:36 +0800

    update picture
    update path

--- picture/gif.py
+++ picture/gif.py
@@ -9,7 +9,7 @@ from . import logger
 USER_AGENT = """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/\
 537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"""
 
-IMAGES_PATH = "/media/pi/other/images"
+IMAGES_PATH = "/media/share/other/images"
 
 open_exceptions = (
     requests.exceptions.ReadTimeout,
