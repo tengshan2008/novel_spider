bdf81708f8763c17f0f9d81ac31e9b7a723c3056
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-03-19 15:40:11 +0800

    update path

--- picture/gif.py
+++ picture/gif.py
@@ -10,7 +10,7 @@ USER_AGENT = """Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML,\
  like Gecko) Raspbian Chromium/78.0.3904.108 Chrome/78.0.3904.108 Safari/5\
 37.36"""
 
-IMAGES_PATH = "/tmp"
+IMAGES_PATH = "/media/share/other/images"
 
 open_exceptions = (
     requests.exceptions.ReadTimeout,
