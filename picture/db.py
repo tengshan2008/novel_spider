47d32beb5c3d7cb946f785c3c6a2d5b98ef94fc5
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-03-20 18:17:48 +0800

    update picture
    update error log

---
+++ picture/db.py
@@ -0,0 +1,18 @@
+import sqlite3
+from pathlib import Path
+
+base_path = Path(__file__).parent
+
+sql_read = """
+"""
+
+sql_insert = """
+"""
+
+sql_delete = """
+"""
+
+
+class Database(object):
+    def __init__(self):
+        pass
