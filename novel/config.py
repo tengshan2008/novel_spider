9fd52101dd6a97a3145fe91cd9661b072b9fc21d
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-03-20 16:52:40 +0800

    update config
    update host

--- novel/config.py
+++ novel/config.py
@@ -1,5 +1,5 @@
 # web
-HOST = "https://cl.330f.tk"
+HOST = "https://cb.333a.ml"
 USER_AGENT = """Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/\
 537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A"""
 
