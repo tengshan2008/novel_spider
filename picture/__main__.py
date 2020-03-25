e3274da83cf9c172c66f412766b6b1707e98a22c
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-03-25 18:00:24 +0800

    update picture
    update check

--- picture/__main__.py
+++ picture/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 
-from . import db, gif
+from . import db, gif, check
 from .config import DB_FILE
 
 
@@ -9,7 +9,13 @@ def cmd():
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("-l", "--link", type=str,
                         help="blog link of picture")
+    parser.add_argument("-c", "--check", action="store_true",
+                        help="check miss picture")
     args = parser.parse_args()
+
+    if args.check:
+        check.check_all()
+        return
 
     database = db.Database(DB_FILE)
     database.init()
