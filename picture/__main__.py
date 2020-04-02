e359da972a559a75162f5da884e8f57fa92970a7
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-04-02 15:51:27 +0800

    update picture
    update daemon

--- picture/__main__.py
+++ picture/__main__.py
@@ -1,27 +1,7 @@
 import argparse
-from multiprocessing import Process
-
-from daemon import runner
 
 from . import check, db, gif
 from .config import DB_FILE
-
-
-class App(object):
-    def __init__(self, url):
-        self.url = url
-        self.stdin_path = '/dev/null'
-        self.stdout_path = '/dev/tty'
-        self.stderr_path = '/tmp/picture_error.log'
-        self.pidfile_path = '/tmp/picture.pid'
-        self.pidfile_timeout = 5
-
-    def run(self):
-        database = db.Database(DB_FILE)
-        database.init()
-        page = gif.Page(self.url)
-        page.parse()
-        page.download()
 
 
 def cmd():
@@ -33,20 +13,21 @@ def cmd():
                         help="check miss picture")
     parser.add_argument("-d", "--daemon", action="store_true",
                         help="daemon mode")
-    parser.add_argument("start")
     args = parser.parse_args()
-
-    app = App(args.link)
 
     if args.check:
         check.check_all()
         return
     if args.daemon:
-        daemon_runner = runner.DaemonRunner(app)
-        daemon_runner.do_action()
+        # daemon_runner = runner.DaemonRunner(app)
+        # daemon_runner.do_action()
         return
 
-    app.run()
+    database = db.Database(DB_FILE)
+    database.init()
+    page = gif.Page(args.link)
+    page.parse()
+    page.download()
 
 
 if __name__ == "__main__":
