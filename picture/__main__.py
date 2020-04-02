f7f8cc99a0d468f3c5ce65f537ca96a2a14ecc84
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-04-02 14:14:01 +0800

    update picture
    update daemon mode

--- picture/__main__.py
+++ picture/__main__.py
@@ -1,7 +1,16 @@
 import argparse
+from multiprocessing import Process
 
 from . import db, gif, check
 from .config import DB_FILE
+
+
+def run(url):
+    database = db.Database(DB_FILE)
+    database.init()
+    page = gif.Page(url)
+    page.parse()
+    page.download()
 
 
 def cmd():
@@ -11,17 +20,24 @@ def cmd():
                         help="blog link of picture")
     parser.add_argument("-c", "--check", action="store_true",
                         help="check miss picture")
+    parser.add_argument("-d", "--daemon", action="store_true",
+                        help="daemon mode")
     args = parser.parse_args()
 
     if args.check:
         check.check_all()
         return
+    if args.daemon:
+        backend(args.link)
+        return
 
-    database = db.Database(DB_FILE)
-    database.init()
-    page = gif.Page(args.link)
-    page.parse()
-    page.download()
+    run(args.link)
+
+
+def backend(url):
+    p = Process(target=run, args=(url,))
+    p.daemon = True
+    p.start()
 
 
 if __name__ == "__main__":
