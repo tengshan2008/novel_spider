0d7366d19c92a8556091e855dce9cc13cd6b03e1
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-04-21 10:22:49 +0800

    update novel
    update config
    
    update host

--- novel/config.py
+++ novel/config.py
@@ -1,5 +1,5 @@
 # web
-HOST = "https://cl.dn37.xyz"
+HOST = "https://cl.hn32.xyz"
 USER_AGENT = """Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit\
 /535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1"""
 
