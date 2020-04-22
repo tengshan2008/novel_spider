d11c37d5d95db4dddff6c496e882d3c69befd17f
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-04-22 16:37:39 +0800

    update tests
    update web online tool

--- tests/test_normal.py
+++ tests/test_normal.py
@@ -1,6 +1,22 @@
 from mechanicalsoup import StatefulBrowser as Browser
 
 
+def filter_title(title):
+    # Invaild symbol \ / : * ? < > |
+    title = title.replace("\\", '')
+    title = title.replace('/', '')
+    title = title.replace(':', '：')
+    title = title.replace('*', '')
+    title = title.replace('?', '？')
+    title = title.replace('<', '《')
+    title = title.replace('>', '》')
+    title = title.replace('|', '')
+    title = title.replace('(', '（')
+    title = title.replace(')', '）')
+    title = title.replace(' ', '')
+    return title
+
+
 def open_html(url):
     soup = None
     with Browser() as browser:
@@ -18,13 +34,16 @@ def video_frame(url):
     soup = open_html(url)
     main = soup.body.find_all("div", id="main", recursive=False)[0]
     tpc_cont = main.find_all("div", class_="tpc_cont", recursive=False)[0]
+    form = main.find_all("form", recursive=False)[0]
+    input_class = form.div.table.find_all("input", class_="input")[0]
+    title = filter_title(input_class['value'])
     _, in_a = tpc_cont.find_all("a", recursive=False)
     source_link = in_a["onclick"].split('=')[1][1:-12]
-    return source_link
+    return title, source_link
 
 
 if __name__ == "__main__":
-    url = "http://cl.hn32.xyz/htm_mob/2004/22/3890174.html"
-    source_link = video_frame(url)
-    print(source_link)
+    url = "http://cl.dn37.xyz/htm_mob/2001/22/3775997.html"
+    title, source_link = video_frame(url)
+    print(f"you-get -d {source_link} -O \"{title}\"")
     # video_source(source_link)
