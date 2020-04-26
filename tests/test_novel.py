026e682be6fe58e3347d1daf755ecf3e01d1fec5
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-04-26 10:24:48 +0800

    update tests

--- tests/test_novel.py
+++ tests/test_novel.py
@@ -3,13 +3,13 @@ from context import book, fix, crawel
 
 def book_test():
     info = {
-        "id": 1191813,
-        "title": "老江吃嫩草[完结]",
-        "author": "壹零贰肆",
-        "date": "2014-09-08",
-        "type": "[現代奇幻]",
-        "link": "https://cb.321i.xyz/htm_data/1409/20/1191813.html",
-        "pages": 28
+        "id": 1193082,
+        "title": "大唐双龙之重生边不负（1-36章+大结局）[完]",
+        "author": "moxx",
+        "date": "2014-09-10",
+        "type": "[古典武俠]",
+        "link": "https://cb.321i.xyz/htm_data/1409/20/1193082.html",
+        "pages": 29
     }
     novel = book.Novel(info['link'], tid=str(info['id']), title=info['title'],
                        date=info['date'], category=info['type'],
