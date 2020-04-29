3c2536ddfb76bba0f65baa3410f6eee5aca65ef4
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-04-29 16:31:15 +0800

    update tests

--- tests/test_novel.py
+++ tests/test_novel.py
@@ -9,7 +9,7 @@ SCKEY = "SCU94031T56e85df7072dc2c313d2f4de1b3ef1315e96c7a1afe53"
 
 
 def book_test():
-    info = {'link': 'https://cl.hn32.xyz/htm_data/1911/20/3706062.html', 'title': '高中同学组织聚餐', 'type': '[現代奇幻]', 'pages': 16, 'author': '战地', 'date': '2019-11-08', 'id': 3706062}
+    info = {'link': 'https://cl.hn32.xyz/htm_data/1911/20/3706059.html', 'title': '被潜规则，应征护士', 'type': '[現代奇幻]', 'pages': 11, 'author': 'Lession', 'date': '2019-11-08', 'id': 3706059}
     novel = book.Novel(info['link'], tid=str(info['id']), title=info['title'],
                        date=info['date'], category=info['type'],
                        author=info['author'], pages=info['pages'])
