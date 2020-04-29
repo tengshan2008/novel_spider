7247e9f32be618c07d23ae9bd86fbc64b807294e
yanghetengshan <yanghetengshan@sayinfo.cn>
2020-04-29 16:12:11 +0800

    update tests

--- tests/test_novel.py
+++ tests/test_novel.py
@@ -9,7 +9,7 @@ SCKEY = "SCU94031T56e85df7072dc2c313d2f4de1b3ef1315e96c7a1afe53"
 
 
 def book_test():
-    info = {'link': 'https://cl.hn32.xyz/htm_data/1406/20/1126430.html', 'title': '新任英语教师[全本11章完]', 'type': '[另類禁忌]', 'pages': 7, 'author': '蒙牛酸酸乳', 'date': '2014-06-08', 'id': 1126430}
+    info = {'link': 'https://cl.hn32.xyz/htm_data/1909/20/3648308.html', 'title': '蝶恋花', 'type': '[UNKOWN]', 'pages': 4, 'author': 'xiaoli07212', 'date': '2019-09-14', 'id': 3648308}
     novel = book.Novel(info['link'], tid=str(info['id']), title=info['title'],
                        date=info['date'], category=info['type'],
                        author=info['author'], pages=info['pages'])
